c9bc4bc6b8751135d6900e185cf54a0cefcc18d6
binodpoudel <footandshadows@gmail.com>
2021-03-16 16:23:46 +0100

    Updates

---
+++ precache-manifest.6c6f467417d44949e3ca788376f2be73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1869b334938702f299e0aa870aa104a9",
+    "url": "/react-portfolio-page/index.html"
+  },
+  {
+    "revision": "4a50805a4336fe77ef20",
+    "url": "/react-portfolio-page/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "f761654cd6a519b15395",
+    "url": "/react-portfolio-page/static/css/main.130dec09.chunk.css"
+  },
+  {
+    "revision": "4a50805a4336fe77ef20",
+    "url": "/react-portfolio-page/static/js/2.69daed27.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/react-portfolio-page/static/js/2.69daed27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f761654cd6a519b15395",
+    "url": "/react-portfolio-page/static/js/main.d8c14b38.chunk.js"
+  },
+  {
+    "revision": "60cd3c3aef9843e3a93e",
+    "url": "/react-portfolio-page/static/js/runtime-main.fd21c956.js"
+  },
+  {
+    "revision": "4ecd457eaa4b3a8362d8aa96d737f4ae",
+    "url": "/react-portfolio-page/static/media/my1.4ecd457e.jpg"
+  }
+]);
