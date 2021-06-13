ea3e6d07fb83de84c69abf148e16e79042d5beb6
Binod <52706075+binodpoudel@users.noreply.github.com>
2021-06-13 21:58:33 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
- 
+ <React.StrictMode>
    <BrowserRouter>
      <App />
-   </BrowserRouter>,
- 
+   </BrowserRouter>
+ </React.StrictMode>,
   document.getElementById('root')
 );
 
